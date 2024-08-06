2a1cd39924682403cf5f5c22f1cacb864c904ff7
Ben Lis <bl2759@columbia.edu>
2024-08-06 12:50:13 -0400

    Removed commented code and unused convert_to_json method

--- foiarchive2json.py
+++ foiarchive2json.py
@@ -1,15 +1,7 @@
 import json
 import psycopg2
 import aiosql
-
-def convert_to_json(list_of_tuples):
-    # Convert the list of tuples to a list of dictionaries
-    list_of_dicts = [dict(tuple_element) for tuple_element in list_of_tuples]
-
-    # Convert the list of dictionaries to JSON
-    json_data = json.dumps(list_of_dicts, indent=2)
-
-    return json_data
+import argparse
 
 conn = psycopg2.connect("")
 stmts = aiosql.from_path("foiarchive.sql", "psycopg2")
@@ -20,7 +12,3 @@ for d in docs:
                               authored=d[3], title=d[4], body=d[5], source=d[6]))
 with open('foiarchive.json', 'w') as j:
     j.write(json.dumps(docs_as_dicts, indent=4, default=str))
-
-# print(dict(ldocs[0]))
-# json_result = convert_to_json(ldocs)
-#print(json_result)
