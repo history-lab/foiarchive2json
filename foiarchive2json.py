49bed3ec260aab2b595dd3ff5d9edf7d81596efc
Ben Lis <bl2759@columbia.edu>
2024-09-03 09:16:43 -0400

    Updated to process foiarchive.docs

--- foiarchive2json.py
+++ foiarchive2json.py
@@ -4,11 +4,16 @@ import aiosql
 import argparse
 
 conn = psycopg2.connect("")
-stmts = aiosql.from_path("foiarchive.sql", "psycopg2")
+# Create a cursor
+cur = conn.cursor()
+cur.execute("SET statement_timeout = 0;")
+stmts = aiosql.from_path("sql/cia-foiarchive.sql", "psycopg2")
 docs = stmts.get_docs(conn)
 docs_as_dicts = []
 for d in docs:
-    docs_as_dicts.append(dict(doc_id=d[0], corpus=d[1], classification=d[2], 
-                              authored=d[3], title=d[4], body=d[5], source=d[6]))
-with open('foiarchive.json', 'w') as j:
+    docs_as_dicts.append(dict(
+        doc_id=d[0], classification=d[1], authored=d[2], 
+        title=d[3], body=d[4], source=d[5],
+        pg_cnt=d[6], char_cnt=d[7], word_cnt=d[8]))
+with open('cia-foiarchive.json', 'w') as j:
     j.write(json.dumps(docs_as_dicts, indent=4, default=str))
