602de66ae085278a30cf52411f6c91bdb3db381e
Ben Lis <bl2759@columbia.edu>
2024-09-04 07:23:12 -0400

    Refactored from array of dicts to single dict inside for loop

--- foiarchive2json.py
+++ foiarchive2json.py
@@ -9,11 +9,9 @@ cur = conn.cursor()
 cur.execute("SET statement_timeout = 0;")
 stmts = aiosql.from_path("sql/cia-foiarchive.sql", "psycopg2")
 docs = stmts.get_docs(conn)
-docs_as_dicts = []
 for d in docs:
-    docs_as_dicts.append(dict(
-        doc_id=d[0], classification=d[1], authored=d[2], 
-        title=d[3], body=d[4], source=d[5],
-        pg_cnt=d[6], char_cnt=d[7], word_cnt=d[8]))
-    with open('cia-foiarchive-pre1970.json', 'a') as j:
-        j.write(json.dumps(docs_as_dicts, indent=4, default=str))
+    doc_dict = dict(doc_id=d[0], classification=d[1], authored=d[2], 
+                    title=d[3], body=d[4], source=d[5],
+                    pg_cnt=d[6], char_cnt=d[7], word_cnt=d[8])
+    with open('cia-foiarchive.json', 'a') as j:
+        j.write(json.dumps(doc_dict, default=str) + '\n')
